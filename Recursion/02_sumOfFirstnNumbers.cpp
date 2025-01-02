58807389a7d678c81b04d0bcdafdea3e473311d5
Lokesh <lokeshchauhan7729@gmail.com>
2025-01-03 00:59:59 +0530

    sum of n numbers

--- Recursion/02_sumOfFirstnNumbers.cpp
+++ Recursion/02_sumOfFirstnNumbers.cpp
@@ -1,25 +1,32 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void func(int i, int sum)
-{
+void func(int i, int sum){
+   
+   // Base Condition.
+   if(i<1)
+   {
+       cout<<sum<<endl;
+       return;
+   }
 
-    // Base Condition.
-    if (i < 1)
-    {
-        cout << sum << endl;
-        return;
-    }
+   // Function call to increment sum by i till i decrements to 1.
+   func(i-1,sum+i);
 
-    // Function call to increment sum by i till i decrements to 1.
-    func(i - 1, sum + i);
 }
 
-int main()
-{
-
-    // Here, let’s take the value of n to be 3.
-    int n = 3;
-    func(n, 0);
+int functional(int n){
+  if(n==0){
     return 0;
+  }
+  return n + functional(n-1);
+
+}
+int main(){
+  
+  // Here, let’s take the value of n to be 3.
+  int n = 3;
+  // func(n,0);
+  cout<<functional(n)<<endl;
+  return 0;
 }
